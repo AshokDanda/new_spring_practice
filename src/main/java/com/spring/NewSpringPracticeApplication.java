59fa6642fa62315311222150a02dfaf07eec3bb7
AshokDanda <ashokdanda97@gmail.com>
2023-09-13 12:23:09 +0530

    first commit

---
+++ src/main/java/com/spring/NewSpringPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringPracticeApplication.class, args);
+	}
+
+}
